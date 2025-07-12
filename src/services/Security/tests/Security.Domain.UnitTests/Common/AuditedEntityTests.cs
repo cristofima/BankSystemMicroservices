ff686c7c62aec67ed5818c6e18b5b3ba947871ca
Cristopher Coronado Moreira <cristofima@hotmail.com>
2025-07-12 09:52:20 -0500

    test(Security.Domain.UnitTests): simplify AuditedEntityTests by removing redundant tests

--- src/services/Security/tests/Security.Domain.UnitTests/Common/AuditedEntityTests.cs
+++ src/services/Security/tests/Security.Domain.UnitTests/Common/AuditedEntityTests.cs
@@ -10,52 +10,13 @@ public class AuditedEntityTests
     }
 
     [Fact]
-    public void Constructor_ShouldSetCreatedAtToCurrentTime()
-    {
-        // Arrange
-        var beforeCreation = DateTime.UtcNow;
-
-        // Act
-        var entity = new TestAuditedEntity();
-        var afterCreation = DateTime.UtcNow;
-
-        // Assert
-        entity.CreatedAt.Should().BeOnOrAfter(beforeCreation);
-        entity.CreatedAt.Should().BeOnOrBefore(afterCreation);
-    }
-
-    [Fact]
-    public void Constructor_ShouldInitializeUpdatedAtAsNull()
+    public void Constructor_ShouldInitializePropertiesAsNull()
     {
         // Act
         var entity = new TestAuditedEntity();
 
         // Assert
         entity.UpdatedAt.Should().BeNull();
-    }
-
-    [Fact]
-    public void UpdatedAt_Setter_ShouldSetValue()
-    {
-        // Arrange
-        var testDate = new DateTime(2023, 6, 15, 14, 30, 0, DateTimeKind.Utc);
-        var entity = new TestAuditedEntity
-        {
-            // Act
-            UpdatedAt = testDate
-        };
-
-        // Assert
-        entity.UpdatedAt.Should().Be(testDate);
-    }
-
-    [Fact]
-    public void Constructor_ShouldInitializeCreatedByAndUpdatedByAsNull()
-    {
-        // Act
-        var entity = new TestAuditedEntity();
-
-        // Assert
         entity.CreatedBy.Should().BeNull();
         entity.UpdatedBy.Should().BeNull();
     }
