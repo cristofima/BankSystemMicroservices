55f3835cbccb3ee2658fff4f8e4268f9fadc8054
Cristopher Coronado Moreira <cristofima@hotmail.com>
2025-08-03 11:00:35 -0500

    feat(aspire-app): add local development orchestration and monitoring setup

---
+++ src/aspire-app/AppHost/AppHost.cs
@@ -0,0 +1,15 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+// Projects
+var security = builder
+    .AddProject<Projects.Security_Api>("security");
+
+var account = builder
+    .AddProject<Projects.BankSystem_Account_Api>("account");
+
+builder
+    .AddProject<Projects.BankSystem_ApiGateway>("api-gateway")
+    .WaitFor(security)
+    .WaitFor(account);
+
+builder.Build().Run();
